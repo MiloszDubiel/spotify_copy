08287853263fc8a0bff9592e501ea2c02d406182
Miłosz Dubiel <syler2871@wp.pl>
2024-06-30 20:00:12 +0200

    'v 0.0.1a'

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -10,8 +10,10 @@ const Deatils = props => {
                 <div className={styles.buttonsDiv}>
                     <button onClick={()=>{
                         document.querySelector(`.${styles.details}`).classList.toggle(`${styles.hidden}`)
+                        document.querySelector(`.${styles.buttonsDiv}`).classList.toggle(`${styles.changePosition}`)
+                        document.querySelector('#details').classList.toggle(`${styles.rotate}`)
                     }}>
-                        <svg style={{cursor: 'pointer'}} class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <svg id="details" style={{cursor: 'pointer'}} class="w-6 h-6 text-gray-800 dark:text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" color="#ffffff" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7"/>
                         </svg>
                     </button>
